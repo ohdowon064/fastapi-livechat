b38485485d0b0fea4f6e8ea45226e98c36796ed7
Dowon Oh <5d1@hits.ai>
2022-05-08 16:43:06 +0900

    disconnection handle and multiple clients connection

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from typing import Optional
-from fastapi import Cookie, Depends, FastAPI, Query, status
+from typing import List, Optional
+from fastapi import Cookie, Depends, FastAPI, Query, WebSocketDisconnect, status
 from fastapi.websockets import WebSocket
 from fastapi.requests import Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import uvicorn
+import websockets
 
 app = FastAPI()
 
@@ -15,31 +16,42 @@ templates = Jinja2Templates(directory="templates")
 async def root(request: Request):
     return templates.TemplateResponse("chat.html", {"request": request})
 
-async def get_cookie_or_token(
-    websocket: WebSocket,
-    session: Optional[str] = Cookie(None),
-    token: Optional[str] = Query(None),    
-):
-    if session or token:
-        return session or token
-    await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
-
-@app.websocket("/items/{item_id}/ws")
-async def websocket_depends(
-    websocket: WebSocket,
-    item_id: str,
-    q: Optional[str] = None,
-    cookie_or_token: str = Depends(get_cookie_or_token)
-):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"cookie or token value is: {cookie_or_token}")
+class ConnectionManager:
+    def __init__(self) -> None:
+        self.active_connections: List[WebSocket] = []
+    
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
         
-        if q:
-            await websocket.send_text(f"query param value is: {q}")
-            
-        await websocket.send_text(f"Message text was: {data}, for item ID: {item_id}")
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+        
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+    
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+manager = ConnectionManager()
+
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: str):
+    await manager.connect(websocket)
+    await manager.broadcast(f"{client_id} Into the chat room")
+    
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.broadcast(f"{client_id} send a message: {data}")
+            await manager.send_personal_message(f"Server reply {client_id}: The message you sent is: {data}", websocket)
+    
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"{client_id} Left the chat room")
+    
+
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", reload=True, host="127.0.0.1", port=8000)
